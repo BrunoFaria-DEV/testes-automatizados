9c1727c5fde45273d7cf6a2e25b3db7f3c555143
BrunoFaria-DEV <developer.brunofaria@gmail.com>
2025-06-18 20:50:01 -0400

    update project

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/bruno/Sites/DJANGO/testes-automatizados/venv
+set -gx VIRTUAL_ENV /home/bruno/Sites/PYTHON/BrunoFaria-DEV/testes-automatizados/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
