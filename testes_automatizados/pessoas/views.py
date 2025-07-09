b84a40d8695dc0260943cbef914030bf539ca28a
BrunoFaria-DEV <developer.brunofaria@gmail.com>
2025-07-09 19:12:32 -0400

    lógica das views

--- testes_automatizados/pessoas/views.py
+++ testes_automatizados/pessoas/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from pessoas.models import Pessoa
 from .forms import PessoaForm
 
 def lista_pessoas(request):
-    return render(request, 'pessoas/lista.html', {'pessoas': []})
+    pessoas = Pessoa.objects.all()
+    return render(request, 'pessoas/lista.html', {'pessoas': pessoas})
 
 def criar_pessoa(request):
-    form = PessoaForm()
+    if request.method == 'POST':
+        form = PessoaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_pessoas')
+    else:
+        form = PessoaForm()
     return render(request, 'pessoas/form.html', {'form': form})
 
 def editar_pessoa(request, id):
-    form = PessoaForm()
+    pessoa = get_object_or_404(Pessoa, id=id)
+    if request.method == 'POST':
+        form = PessoaForm(request.POST, instance=pessoa)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_pessoas')
+    else:
+        form = PessoaForm(instance=pessoa)
     return render(request, 'pessoas/form.html', {'form': form})
 
 def excluir_pessoa(request, id):
-    return render(request, 'pessoas/confirmar_exclusao.html', {})
+    pessoa = get_object_or_404(Pessoa, id=id)
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect('lista_pessoas')
+    return render(request, 'pessoas/confirmar_exclusao.html', {'pessoa': pessoa})
